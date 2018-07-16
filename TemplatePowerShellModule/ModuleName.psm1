3502bd9c53a52185c0c369f8a2e6e602478c46a7
Josh Rickard <josh.rickard@phishme.com>
2018-07-15 21:06:12 -0400

    Added logic to import Classes in PSM1

--- TemplatePowerShellModule/ModuleName.psm1
+++ TemplatePowerShellModule/ModuleName.psm1
@@ -1,3 +1,7 @@
+# using module .\TemplatePowerShellModule\Class\TemplatePowerShellModule.Class1.psm1
+# Above needs to remain the first line to import Classes
+# remove the comment when using classes
+
 #requires -Version 2
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
